ab2b729d26cc1b111b7346bbbb1300fd0f28607c
Weezele <wesamnazzal8@outlook.com>
2023-10-10 00:18:31 +0300

    .

--- BlogApplication.UI/Controllers/AdminTagsController.cs
+++ BlogApplication.UI/Controllers/AdminTagsController.cs
@@ -1,13 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlogApplication.UI.Data;
+using BlogApplication.UI.Models.Domain;
+using BlogApplication.UI.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApplication.UI.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private BlogDbContext dbContext;
+        public AdminTagsController(BlogDbContext blogDbContext)
+        {
+            dbContext = blogDbContext;
+        }
+
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            // Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            dbContext.Tags.Add(tag);
+            dbContext.SaveChanges();
+
+            return View("Add");
         }
     }
 }
