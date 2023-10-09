9f3703b8fba745b451661fd5d3a7a5537069dd2c
Weezele <wesamnazzal8@outlook.com>
2023-10-09 20:56:55 +0300

    Tag Model

---
+++ BlogApplication.UI/Models/Domain/Tag.cs
@@ -0,0 +1,13 @@
+﻿namespace BlogApplication.UI.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? DisplayName { get; set; }
+
+
+    }
+}
