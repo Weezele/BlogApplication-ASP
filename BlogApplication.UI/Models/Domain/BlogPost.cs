142feb5f745c4ee1a70e399d421741582ba5837d
Weezele <wesamnazzal8@outlook.com>
2023-10-09 20:54:18 +0300

    Blog Post Model

---
+++ BlogApplication.UI/Models/Domain/BlogPost.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApplication.UI.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+
+        public string? Heading { get; set; }
+
+        public string? PageTitle { get; set; }
+
+        public string? Content { get; set; }
+
+        public string? ShortDescription { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public string? UrlHandler { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public string? Author { get; set; }
+
+        public bool Visible { get; set; }
+    }
+}
