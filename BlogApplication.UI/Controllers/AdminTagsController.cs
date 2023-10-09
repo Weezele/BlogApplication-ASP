c7e0d9be994c6d4ce601292bd8d88230b9da8993
Weezele <wesamnazzal8@outlook.com>
2023-10-09 23:20:48 +0300

    Add Tag

---
+++ BlogApplication.UI/Controllers/AdminTagsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApplication.UI.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
