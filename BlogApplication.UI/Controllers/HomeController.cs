5327a3ab42b6054e9b929e826b727b2fb23409a2
Weezele <wesamnazzal8@outlook.com>
2023-10-11 20:03:02 +0300

    adding designs

--- BlogApplication.UI/Controllers/HomeController.cs
+++ BlogApplication.UI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using BlogApplication.UI.Models;
+using BlogApplication.UI.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +8,19 @@ namespace BlogApplication.UI.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBlogPostRepository blogPostRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository)
         {
             _logger = logger;
+            this.blogPostRepository = blogPostRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var blogPost = await blogPostRepository.GetAllAsync();
+
+            return View(blogPost);
         }
 
         public IActionResult Privacy()
